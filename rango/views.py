e3ad6d27dd0dec8697edfaad2239191aaa54978f
Joseph Beattie <josephbeattiee@gmail.com>
2026-02-24 13:52:10 +0000

    (Chapter 4.x) - Added cat image, new about HTML template, media folders and references in settings. Completed Chapter 4. All tests pass.

--- rango/views.py
+++ rango/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    context_dict = {'boldmessage': 'Crunch, creamy, cookie, candy, cupcake!'}
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+    context_dict = {'boldmessage': 'This tutorial has been put together by Joseph Beattie.'}
+    return render(request, 'rango/about.html', context=context_dict)
