1b1d34076d54da0a124b88d7802c4407b5075fdb
Joseph Beattie <josephbeattiee@gmail.com>
2026-02-25 08:26:25 +0000

    (Chapter 5.x) - All exercises completed. Now contains more complete table for page information in admin, views and likes added like the screenshot given in the Tango with Django manual

--- populate_rango.py
+++ populate_rango.py
@@ -1,4 +1,7 @@
 import os
+
+from rango import views
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tango_with_django_project.settings")
 
 import django
@@ -24,15 +27,29 @@ def populate():
         {'title': 'Flask', 'url': 'http://flask.pocoo.org/'}
     ]
 
-    cats = {'Python' : {'pages': python_pages},
-            'Django' : {'pages': django_pages},
-            'Other Frameworks' : {'pages': other_pages}}
+    cats = {
+        'Python' : {
+            'pages': python_pages,
+            'views': 128,
+            'likes': 64
+        },
+        'Django' : {
+            'pages': django_pages,
+            'views': 64,
+            'likes': 32
+        },
+        'Other Frameworks' : {
+            'pages': other_pages,
+            'views': 32,
+            'likes': 16
+        }
+    }
 
     ## Joseph note - add categories / pages above (cats for categories and pages etc
 
     ## Cycling through dictionary below...
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        c = add_cat(cat, cat_data.get('views', 0), cat_data.get('likes', 0))
         for p in cat_data['pages']:
             add_page(c, p['title'], p['url'])
 
@@ -49,8 +66,10 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name):
+def add_cat(name, views=0, likes=0):
     c = Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.likes = likes
     c.save()
     return c
 
