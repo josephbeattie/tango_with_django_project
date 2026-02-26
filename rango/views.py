6056e7d7864d548cb25e787766b74f24666d4d12
Joseph Beattie <josephbeattiee@gmail.com>
2026-02-26 08:23:10 +0000

    (Chapter 10.x) - Completed Chapter 10. Looked at and implemented server side cookie functionality, as well as a site visit ticker on the about page

--- rango/views.py
+++ rango/views.py
@@ -13,6 +13,9 @@ from rango.models import Page
 from rango.forms import CategoryForm, UserForm, UserProfileForm
 from rango.forms import PageForm
 
+## Misc imports
+from datetime import datetime
+
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
@@ -22,12 +25,18 @@ def index(request):
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
 
+    visitor_cookie_handler(request)
+
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    context_dict = {'boldmessage': 'This tutorial has been put together by Joseph Beattie.'}
-    return render(request, 'rango/about.html', context=context_dict)
+    context_dict = {}
 
+    visitor_cookie_handler(request)
+    context_dict['boldmessage'] = 'This tutorial has been put together by Joseph Beattie.'
+    context_dict['visits'] = request.session['visits']
+
+    return render(request, 'rango/about.html', context=context_dict)
 
 def show_category(request, category_name_slug):
     context_dict = {}
@@ -155,3 +164,28 @@ def restricted(request):
 def user_logout(request):
     logout(request)
     return redirect(reverse('rango:index'))
+
+
+## COOKIES... mmmm cookies
+
+### helper for server side cookies
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    ## If more than a day since last visit
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+
+    request.session['visits'] = visits
