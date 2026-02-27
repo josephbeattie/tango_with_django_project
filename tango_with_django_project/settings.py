f9871ae4afc977d9669f288a45f83e6395be9589
Joseph Beattie <josephbeattiee@gmail.com>
2026-02-27 14:28:56 +0000

    (Chapter 11.x) - Added django-registration-redux package and linked admin auth tools to the main HTML templates. Users can now change passwords, register and login with the new views

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rango',
+    'registration',
 ]
 
 MIDDLEWARE = [
@@ -137,3 +138,12 @@ STATICFILES_DIRS = [STATIC_DIR, ]
 
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
+
+## Registration area
+REGISTRATION_OPEN = True
+
+REGISTRATION_AUTO_LOGIN = True
+
+LOGIN_REDIRECT_URL = 'rango:index'
+
+LOGIN_URL = 'auth_login'
